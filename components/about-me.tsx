bd6b48fffd52ecec417d10aca9b7b53f6da466d3
adryan <42096409+ZeyKra@users.noreply.github.com>
2025-05-11 02:08:22 +0200

    [UPDATE] About me description

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -3,8 +3,8 @@ export function AboutMe() {
     <section className="space-y-4">
       <h2 className="font-feather text-3xl font-bold">À Propos</h2>
       <p className="text-lg text-muted-foreground">
-        Velit aute in nulla occaecat. Aute officia veniam pariatur dolore minim dolor occaecat ipsum. Adipisicing ex deserunt ad culpa occaecat est amet.
-        Sunt amet dolor voluptate deserunt mollit. Ad deserunt laboris id excepteur est laboris duis occaecat est occaecat laboris aliqua exercitation duis. Do veniam do laborum consequat duis eu sunt anim nulla ullamco mollit voluptate officia duis.
+      Actuellement, je poursuis un BTS SIO (Services Informatiques aux Organisations) avec une spécialisation dans le développement d&apos;applications et la gestion de projets informatiques. Étant passionné par les technologies émergentes, je conçois des solutions personnalisées en utilisant des langages tels que HTML, CSS, Typescript, PHP, ainsi que des frameworks comme React.
+      Au fil de ma formation, j&apos;ai développé des compétences solides dans le domaine du développement web, des bases de données (MySQL, SurrealDB) et de la gestion de projets, en me familiarisant avec des méthodologies telles que Kanban et Agile. Mes expériences en entreprise m&apos;ont offert l&apos;opportunité d&apos;apprendre et me familiariser au monde du travail. 
       </p>
     </section>
   )
